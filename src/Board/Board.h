ae3ae29bfa5b67f6a656ab0a3a536d0a44707955
JPSRob <jps.roberts@gmail.com>
2014-11-18 20:27:51 -0500

    small update

--- src/Board/Board.h
+++ src/Board/Board.h
@@ -5,8 +5,8 @@
 class Board{
 public:
     //Constructor: loads string vector map. Converts to a 2D-Array for matrices calculations.
-    //Finds and sets playerPositionX and playerPositionY. Finds and sets goal locations.
-    Board(vector<string>); //!!!!!! Fill in passed variable with string vector !!!!!!!!!!!!!!
+    //Finds and sets player location. Finds and sets goal and star locations.
+    Board(vector<string>);
     //Destructor
     ~Board();
     //Renders game board, based on 2d array map, within camera bounds.
@@ -18,15 +18,18 @@ public:
 
 
 private:
+    //!Preset dimensions
     const int BOARD_WIDTH = 1500; //per pixel
     const int BOARD_HEIGHT = 1530; //per pixel
     const int TILE_WIDTH = 50; //per pixel
     const int TILE_HEIGHT = 85; //per pixel
-    const int CAMERA_WIDTH = 12; //per tile
-    const int CAMERA_HEIGHT = 7; //per tile
+    const int CAMERA_WIDTH = 7; //per tile
+    const int CAMERA_HEIGHT = 12; //per tile
+    //!MatricesLocations variables
     MatricesLocation player; //Keep track of player position in matrices
     vector<MatricesLocation> goal; //Keep track of goal locations in matrices
     vector<MatricesLocation> star; //Keep track of star locations in matrices
+    //!Score tracking variables
 
 };
 
